ccf3f7c81a146a0b6e2b47d9cc8886cdd699a42b
D7703_18 <d7703_18@d7703-18ui-imac.local>
2018-05-03 11:33:35 +0900

    a

--- UISliderTable/ViewController.swift
+++ UISliderTable/ViewController.swift
@@ -8,18 +8,37 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 100
+    }
+    var i=0
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell=tbl.dequeueReusableCell(withIdentifier: "RE", for: indexPath)
+        cell.textLabel?.text=String(i)
+        i=i+1
+        return cell
+    }
+    
 
+    @IBOutlet weak var tbl: UITableView!
+    @IBOutlet weak var mySlide: UISlider!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        tbl.dataSource=self
+        tbl.delegate=self
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    @IBAction func sliderMove(_ sender: Any) {
+        tbl.reloadData()
+    }
+    
 
 }
 
